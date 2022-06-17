9a24f0db832313446822a5850f435d19395199e2
Katelind Phillips <scarlettkate94@gmail.com>
2022-06-17 14:30:57 -0400

    final ex

---
+++ FactoryPattern/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace FactoryPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int wheelCount;
+            bool input = false;
+
+            do
+            {
+                Console.WriteLine("Enter the amount of tires your new vehicle has:");
+                input = int.TryParse(Console.ReadLine(), out wheelCount);
+            }
+            while (input ==  false);
+
+            var vehicle = VehicleFactory.GetVehicle(wheelCount);
+            vehicle.Drive();
+
+        }
+    }
+}
+
